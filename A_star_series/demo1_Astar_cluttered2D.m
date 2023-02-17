af53e128d63e9cb4e45b75e5082a526a6474e1e0
TangKai-HIT <1292737216@qq.com>
2023-02-17 12:06:23 +0800

    new demos

---
+++ A_star_series/demo1_Astar_cluttered2D.m
@@ -0,0 +1,46 @@
+%demo1: A* in cluttered 2D environment 
+addpath(genpath("../"));
+
+%% Create environment
+% basics
+bbox = [0 1 0 1]; %unit bounding box
+resolution = 0.01; %map resolution
+% obstacle shape
+vertical_disp = [0.4 0.7 0.2 0.5 1.0];
+wall_width = 0.02;
+gap_clearance = 0.1;
+rectangle_array = get_left_right_tunnel( bbox, vertical_disp, wall_width, gap_clearance);
+% convert to map
+map = convert_rectangle_shape_array_to_map( rectangle_array, bbox, resolution ); %coordinate (x,y) = map.table(x+1, y+1)
+% show map
+figure(Position=[300, 100, 800, 800]);
+visualize_map(map);
+axis equal
+axis(bbox)
+
+%% Define start, goal & action set
+start = [0.1, 0.1] / resolution + 1;
+goal = [0.9, 0.9] / resolution + 1;
+action_set.actions = {[1, 1], [-1, -1], [-1, 1], [1, -1], ... 
+                                        [0, 1], [1, 0], [0, -1], [-1, 0]};
+action_set.cost = zeros(1, 8);
+action_set.cost(1:4) = sqrt(2);
+action_set.cost(5:8) = 1;
+
+%% Start planning
+weight = 1; % heuristic weight
+heuristicFcn = @(cur, goal) getHeuristicCost2D(cur, goal, weight);
+disp("Start A* algorithm ...");
+tic;
+pathNodeList = Astar_plan2D(start, goal, map.table, action_set, heuristicFcn);
+solve_time = toc;
+fprintf("Solve time: %.3f s\n", solve_time);
+
+%% Show result plot
+for node = pathNodeList
+    map.table(node.coordinate(1), node.coordinate(2)) = 0.5;
+end
+
+visualize_map(map);
+axis equal
+axis(bbox)
---
+++ A_star_series/demo2_Astar_wall_gaps2D.m
@@ -0,0 +1,43 @@
+%demo2: A* in 2D environment of walls with gaps
+addpath(genpath("../"));
+
+%% Create environment
+% basics
+bbox = [0 1 0 1]; %unit bounding box
+resolution = 0.01; %map resolution
+% obstacle shape
+rectangle_array1 = get_wall_with_uniform_gaps( bbox, 0.3, 0.2, 0.1, [1 1 0 1] );
+rectangle_array2 = get_wall_with_uniform_gaps( bbox, 0.7, 0.2, 0.1, [1 0 1 1] );
+rectangle_array = [rectangle_array1 rectangle_array2];
+% convert to map
+map = convert_rectangle_shape_array_to_map( rectangle_array, bbox, resolution ); %coordinate (x,y) = map.table(x+1, y+1)
+% show map
+figure(Position=[300, 100, 800, 800]);
+visualize_map(map);
+axis equal
+axis(bbox)
+
+%% Define start, goal & action set
+start = [0.1, 0.5] / resolution + 1;
+goal = [0.9, 0.6] / resolution + 1;
+action_set.actions = {[1, 1], [-1, -1], [-1, 1], [1, -1], ... 
+                                        [0, 1], [1, 0], [0, -1], [-1, 0]};
+action_set.cost = zeros(1, 8);
+action_set.cost(1:4) = sqrt(2);
+action_set.cost(5:8) = 1;
+
+%% Start planning
+disp("Start A* algorithm ...");
+tic;
+pathNodeList = Astar_plan2D(start, goal, map.table, action_set, @getHeuristicCost2D);
+solve_time = toc;
+fprintf("Solve time: %.3f s\n", solve_time);
+
+%% Show result plot
+for node = pathNodeList
+    map.table(node.coordinate(1), node.coordinate(2)) = 0.5;
+end
+
+visualize_map(map);
+axis equal
+axis(bbox)
